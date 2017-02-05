9bf6f3e7f607b57d96ffd8fd5e716a2c0b6b4055
Yaroslav <y.petryk87@ukr.net>
2017-02-05 12:52:29 +0000

    edit photo

--- js/holiday.js
+++ js/holiday.js
@@ -1,17 +1,17 @@
+document.getElementById('mainselect').addEventListener('change', selectCity);
 
-document.getElementById('mainselect').addEventListener('change' , selectCity);
 function selectCity() {
 
-    var mainSelect = document.getElementById('mainselect') ;
-    var subSelectId = mainSelect.options[mainSelect.selectedIndex].value+'-subselect' ;
+    var mainSelect = document.getElementById('mainselect');
+    var subSelectId = mainSelect.options[mainSelect.selectedIndex].value + '-subselect';
     var list = document.getElementsByClassName('subselect');
     var newSelect = document.getElementById(subSelectId);
-    
-        for (var i=0; i<list.length; i++) {
-            list[i].style.display = 'none';
-        };
-        
-    newSelect.style.display = 'inline-block' ;
+
+    for (var i = 0; i < list.length; i++) {
+        list[i].style.display = 'none';
+    };
+
+    newSelect.style.display = 'inline-block';
 };
 
 
@@ -19,18 +19,18 @@ function selectCity() {
 
 var select = document.getElementsByTagName('select');
 var i = select.length;
-while(i--) {
-    
-    select[i].addEventListener('change' , showImage);
+while (i--) {
+
+    select[i].addEventListener('change', showImage);
 };
 
 function showImage() {
-    
-    if(this.value!='default') {
-    document.getElementById('city').src = './image/' + this.value + '.jpg';}
-    else { document.getElementById('city').src = './image/pointer.jpg';}
-    
-};  
 
+    if (this.value != 'default') {
+        document.getElementById('city').src = './image/' + this.value + '.jpg';
+    }
+    else {
+        document.getElementById('city').src = './image/pointer.jpg';
+    }
 
-
+};
